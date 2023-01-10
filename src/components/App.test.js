9c8c5e3a76892070f86689d483537ff0cafc563f
mattand <mjande@umich.edu>
2023-01-10 17:30:42 -0500

    Add test for generating games on initial page load

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders today's games on initial page load", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const games = screen.getByRole("game");
+  expect(games).toHaveLength(25);
 });

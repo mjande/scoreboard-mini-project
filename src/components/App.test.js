a7da88f957645a0407c62c7e1dcdd9caf6686ab1
mattand <mjande@umich.edu>
2023-01-10 17:31:12 -0500

    Start setting up API mocking

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// TODO: Move API mocks and mock data to separate file
+function setupFetchStub(data) {
+  return function fetchStub(url) {
+    return new Promise((resolve) => {
+      resolve({
+        json: () => Promise.resolve({ data }),
+      });
+    });
+  };
+}
+
+// jest.spyOn(global, "fetch").mockImplementation(setupFetchStub(fakeData));
+
 test("renders today's games on initial page load", () => {
   render(<App />);
   const games = screen.getByRole("game");
